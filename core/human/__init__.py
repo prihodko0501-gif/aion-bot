e49fbf4da95ce7307a908945208eddaf1770e8d5
prihodko0501-gif <prihodko0501@gmail.com>
2026-03-06 16:49:48 +0300

    Update __init__.py

--- core/human/__init__.py
+++ core/human/__init__.py
@@ -1,1 +1,15 @@
 # AION Human Architecture module
+
+from .physiology import build_physiology
+from .nervous_system import build_nervous_system
+from .adaptation import build_adaptation
+from .recovery_model import build_recovery
+from .human_architecture import build_human_architecture
+
+__all__ = [
+    "build_physiology",
+    "build_nervous_system",
+    "build_adaptation",
+    "build_recovery",
+    "build_human_architecture"
+]
