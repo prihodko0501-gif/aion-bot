721267642fb9d085ae91369f2364a04633c77ff7
prihodko0501-gif <prihodko0501@gmail.com>
2026-03-06 22:07:44 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
+import os
 from flask import Flask, request
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
     return "AION Bot Running"
 
+
 @app.route("/webhook", methods=["POST"])
 def webhook():
     data = request.json
     print(data)
-
     return {"status": "ok"}, 200
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", "10000"))
+    app.run(host="0.0.0.0", port=port)
