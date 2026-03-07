c25e8314d473cc99a5d52b26d402d686498d6590
prihodko0501-gif <prihodko0501@gmail.com>
2026-03-08 00:30:53 +0300

    Update app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "AION BOT WORKING", 200
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", "10000"))
+    print(f"PORT={port}", flush=True)
+    app.run(host="0.0.0.0", port=port)
