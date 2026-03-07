361d04cd5d5b1207a910c7772ab3e40bcd84cb75
prihodko0501-gif <prihodko0501@gmail.com>
2026-03-07 20:55:30 +0300

    Update biotime.py

--- core/biotime.py
+++ core/biotime.py
@@ -1,4 +1,4 @@
-def calculate_biotime(sleep_score, stress_score, pressure_score, recovery_score):
-    # сюда твоя формула
-    biotime = 0
-    return round(biotime, 2)
+# AION Sleep Module
+
+def calculate_sleep(hours, quality):
+    pass
