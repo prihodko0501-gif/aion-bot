c40629d35172b0a657e22a0c5b419ccaad982899
prihodko0501-gif <prihodko0501@gmail.com>
2026-03-07 23:47:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,10 @@
-import os
 from flask import Flask
 
-print("APP.PY STARTED")
-
-from webapp.routes import register_routes
-
-print("ROUTES IMPORTED")
-
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
-register_routes(app)
-
-print("ROUTES REGISTERED")
+@app.route("/")
+def home():
+    return "AION BOT WORKING"
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", "8080"))
-    print("RUNNING ON PORT", port)
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=10000)
