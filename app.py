d05e64fdeac12b87fc454cd65890a6f98f6739c1
prihodko0501-gif <prihodko0501@gmail.com>
2026-03-08 19:47:55 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 import os
 import requests
-from flask import Flask, request, jsonify, send_from_directory
+from flask import Flask, request, send_from_directory
 
 app = Flask(__name__)
 
 TOKEN = os.environ.get("TELEGRAM_TOKEN")
 API = f"https://api.telegram.org/bot{TOKEN}"
 
-IMAGE_URL = "https://raw.githubusercontent.com/prihodko0501-gif/aion-bot/main/B0AEE152-2F0A-4DD9-8A25-D25C1D6AFE54.jpeg"
+IMAGE_URL = "https://raw.githubusercontent.com/prihodko0501-gif/aion-bot/main/B0AEE152-2F0A-4DD9-8A25-D25C1D6AFE54.png"
 
 
 @app.route("/")
 def home():
-    return send_from_directory("webapp", "index.html")
+    return "AION server running"
 
 
 @app.route("/app")
@@ -38,7 +38,7 @@ def webhook():
             json={
                 "chat_id": chat_id,
                 "photo": IMAGE_URL,
-                "caption": "AION\nBiological Upgrade System\n\nСистема инициализирована."
+                "caption": "UPGRADE SYSTEM"
             }
         )
 
