9eba8f73d3c1cd8cd9da6f2e119ada0922631f0c
prihodko0501-gif <prihodko0501@gmail.com>
2026-03-08 03:15:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,60 +7,49 @@ app = Flask(__name__)
 def home():
     return "AION server running"
 
-
 @app.route("/app")
 def mini_app():
-
     html = """
     <html>
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <style>
-
-        body{
-            margin:0;
-            background:#0c0f1a;
-            font-family:Arial;
-            color:white;
-            text-align:center;
-        }
-
-        img{
-            width:100%;
-        }
-
-        .text{
-            padding:25px;
-            font-size:20px;
-            line-height:1.4;
-        }
-
+            body{
+                margin:0;
+                background:#0c0f1a;
+                font-family:Arial;
+                color:white;
+                text-align:center;
+            }
+            img{
+                width:100%;
+                display:block;
+            }
+            .box{
+                padding:20px;
+                font-size:20px;
+                line-height:1.4;
+            }
         </style>
     </head>
-
     <body>
 
         <img src="https://raw.githubusercontent.com/prihodko0501-gif/aion-bot/main/9C9D1C07-7426-4DDC-84DD-A76E6AC20138.png">
 
-        <div class="text">
-
+        <div class="box">
         Система AION разработана как глобальная платформа,
         измеряющая биологическое время и помогающая управлять
         скоростью жизни
-
         </div>
 
     </body>
     </html>
     """
-
     return render_template_string(html)
-
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
     return "ok", 200
-
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
