0cd2835c9c5340156aaadd91b5d29581aa43dad8
prihodko0501-gif <prihodko0501@gmail.com>
2026-03-15 02:02:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,23 +2,20 @@ from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
-# главная страница Mini App
+# Главная страница Mini App
 @app.route("/")
 def index():
     return send_from_directory("webapp", "index.html")
 
-
-# чтобы работали стили, иконки и скрипты
+# Статические файлы (иконки, css, js)
 @app.route("/static/<path:path>")
 def static_files(path):
     return send_from_directory("webapp/static", path)
 
-
-# проверка сервера
+# Проверка работы сервера
 @app.route("/health")
 def health():
     return {"status": "ok"}
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
