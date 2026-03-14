8d13e3b607f7a6f3e372eeedefdcbe642d0956a6
prihodko0501-gif <prihodko0501@gmail.com>
2026-03-14 18:58:21 +0300

    Update aion.js

--- webapp/aion.js
+++ webapp/aion.js
@@ -1,27 +1,13 @@
-if (window.Telegram && Telegram.WebApp) {
-  Telegram.WebApp.ready();
-  Telegram.WebApp.expand();
-  Telegram.WebApp.setHeaderColor('#081a42');
-  Telegram.WebApp.setBackgroundColor('#081a42');
+function enterSystem(){
+window.location="/modules"
 }
 
-function showScreen(id) {
-  document.querySelectorAll('.screen').forEach(screen => {
-    screen.classList.remove('active');
-  });
+async function loadBioTime(){
 
-  const target = document.getElementById(id);
-  if (target) {
-    target.classList.add('active');
-    window.scrollTo(0, 0);
-  }
+let r = await fetch("/api/biotime")
+let data = await r.json()
+
+document.getElementById("biotime").innerText=data.value
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  document.querySelectorAll('[data-open]').forEach(btn => {
-    btn.addEventListener('click', () => {
-      const target = btn.getAttribute('data-open');
-      showScreen(target);
-    });
-  });
-});
+loadBioTime()
