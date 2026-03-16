d545dd45ebecde3e36854e78a3ed8fca062be5ac
prihodko0501-gif <prihodko0501@gmail.com>
2026-03-16 20:48:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -14,19 +14,29 @@ def index():
     return send_from_directory(WEBAPP_DIR, "index.html")
 
 
-@app.route("/static/<path:path>")
-def static_files(path):
-    return send_from_directory(STATIC_DIR, path)
-
-
-@app.route("/test-static")
-def test_static():
-    return send_from_directory(ICONS_DIR, "IMG_1510.png")
-
-
 @app.route("/health")
 def health():
     return {"status": "ok"}
+
+
+@app.route("/screen-1")
+def screen_1():
+    return send_from_directory(ICONS_DIR, "372BD4C9-1164-4136-AF34-97A8E6D5420D.png")
+
+
+@app.route("/screen-2")
+def screen_2():
+    return send_from_directory(ICONS_DIR, "B62761EC-CC31-4EBD-934F-24815970551A.png")
+
+
+@app.route("/screen-3")
+def screen_3():
+    return send_from_directory(ICONS_DIR, "CE9B0085-675C-42C1-98A5-0BF4DD4735EF.png")
+
+
+@app.route("/static/<path:path>")
+def static_files(path):
+    return send_from_directory(STATIC_DIR, path)
 
 
 if __name__ == "__main__":
